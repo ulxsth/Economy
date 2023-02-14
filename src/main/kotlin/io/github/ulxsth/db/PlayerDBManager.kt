3dad474af24919ee6e9216c5e3e8ed940a1a7712
ulxsth <ulxsth@gmail.com>
2023-02-14 15:23:23 +0900

    try-catch節を追加、close処理の追記

--- src/main/kotlin/io/github/ulxsth/db/PlayerDBManager.kt
+++ src/main/kotlin/io/github/ulxsth/db/PlayerDBManager.kt
@@ -11,67 +11,108 @@ import java.sql.SQLException
 import java.util.UUID
 
 class PlayerDBManager {
-    private val connection: Connection
     private val plugin: EconomyPlugin = EconomyPlugin.getInstance()
 
+    private val PATH = "jdbc:sqlite:" + plugin.dataFolder + "\\player.db"
     private val TABLE_NAME = "player"
 
     init {
-        this.connection = DriverManager.getConnection("jdbc:sqlite:" + plugin.dataFolder + "\\player.db")
+        val connection = DriverManager.getConnection(this.PATH)
         val statement = connection.createStatement()
 
         // dbの作成
         statement.executeUpdate("CREATE TABLE IF NOT EXISTS $TABLE_NAME(uuid string primary key, amount integer)")
         this.plugin.logger.info("TABLE CREATED")
+
+        statement.close()
+        connection.close()
     }
 
-    // TODO PreparedStatementを使う
-    // TODO close処理の挿入
     fun create(player: Player) {
-        val playerMoney = player.money
-        val playerUUID = player.bukkitPlayer.uniqueId
+        var connection: Connection? = null
 
-        val sql = "INSERT INTO $TABLE_NAME(uuid, amount) VALUES(?, ?)"
-        val ps = this.connection.prepareStatement(sql)
-        ps.setString(1, playerUUID.toString())
-        ps.setInt(2, playerMoney.amount)
-        ps.executeUpdate()
+        try {
+            connection = DriverManager.getConnection(this.PATH)
+            val playerMoney = player.money
+            val playerUUID = player.bukkitPlayer.uniqueId
 
+            val sql = "INSERT INTO $TABLE_NAME(uuid, amount) VALUES(?, ?)"
+            val ps = connection.prepareStatement(sql)
+            ps.setString(1, playerUUID.toString())
+            ps.setInt(2, playerMoney.amount)
+            ps.executeUpdate()
 
-        val playerName = player.bukkitPlayer.name
-        this.plugin.logger.info("Initialized data: $playerName(uuid: $playerUUID)")
+            val playerName = player.bukkitPlayer.name
+            this.plugin.logger.info("Initialized data: $playerName(uuid: $playerUUID)")
+        } catch (err: SQLException) {
+            err.printStackTrace()
+        } finally {
+            try {
+                connection?.close()
+            } catch (err: SQLException) {
+                err.printStackTrace()
+            }
+        }
+
     }
 
-    fun read(uuid: UUID): Money {
-        val sql = "SELECT amount FROM $TABLE_NAME WHERE uuid = ?"
-        val ps = this.connection.prepareStatement(sql)
-        ps.setString(1, uuid.toString())
-        val rs = ps.executeQuery()
-        rs.next()
+    fun read(uuid: UUID): Money? {
+        var connection: Connection? = null
 
-        val amount = rs.getInt(2)
+        try {
+            connection = DriverManager.getConnection(this.PATH)
+            val sql = "SELECT amount FROM $TABLE_NAME WHERE uuid = ?"
+            val ps = connection.prepareStatement(sql)
+            ps.setString(1, uuid.toString())
+            val rs = ps.executeQuery()
+            rs.next()
 
-        // データが存在するか判定する
-        if(rs.wasNull()) throw SQLException("データが存在しません")
-        val playerMoney = Money(amount)
-        return playerMoney
+            val amount = rs.getInt(2)
+
+            // データが存在するか判定する
+            if (rs.wasNull()) throw SQLException("データが存在しません")
+            val playerMoney = Money(amount)
+            return playerMoney
+        } catch (err: SQLException) {
+            err.printStackTrace()
+        } finally {
+            connection?.close()
+        }
+
+        return null
     }
 
     fun update(player: Player, amount: Int) {
         val playerUUID: UUID = player.bukkitPlayer.uniqueId
+        var connection: Connection? = null
 
-        val sql = "UPDATE $TABLE_NAME SET amount = ? WHERE uuid = ?"
-        val ps = this.connection.prepareStatement(sql)
-        ps.setInt(1, amount)
-        ps.setString(2, playerUUID.toString())
-        ps.executeUpdate()
+        try {
+            connection = DriverManager.getConnection(this.PATH)
+            val sql = "UPDATE $TABLE_NAME SET amount = ? WHERE uuid = ?"
+            val ps = connection.prepareStatement(sql)
+            ps.setInt(1, amount)
+            ps.setString(2, playerUUID.toString())
+            ps.executeUpdate()
+        } catch (err: SQLException) {
+            err.printStackTrace()
+        } finally {
+            connection?.close()
+        }
     }
 
     fun delete(uuid: UUID) {
-        val sql = "DELETE FROM $TABLE_NAME WHERE uuid = ?"
-        val ps = this.connection.prepareStatement(sql)
-        ps.setString(1, uuid.toString())
-        ps.executeUpdate()
+        var connection: Connection? = null
+        try {
+            connection = DriverManager.getConnection(this.PATH)
+            val sql = "DELETE FROM $TABLE_NAME WHERE uuid = ?"
+            val ps = connection.prepareStatement(sql)
+            ps.setString(1, uuid.toString())
+            ps.executeUpdate()
+        } catch (err: SQLException) {
+            err.printStackTrace()
+        } finally {
+            connection?.close()
+        }
     }
 
     fun isExist(uuid: UUID): Boolean {
