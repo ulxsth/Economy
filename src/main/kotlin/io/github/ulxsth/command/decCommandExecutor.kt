f0b3a1f736f39aabd4b3bdc0af243bb700394616
ulxsth <ulxsth@gmail.com>
2023-02-15 14:51:29 +0900

    [ADD] /dec の処理クラスを追加

---
+++ src/main/kotlin/io/github/ulxsth/command/decCommandExecutor.kt
@@ -0,0 +1,12 @@
+package io.github.ulxsth.command
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+class decCommandExecutor: CommandExecutor {
+    @Override
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
